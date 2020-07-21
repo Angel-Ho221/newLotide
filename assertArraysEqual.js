2e836215d6a41acf59f8ab9a524ad0255d03773f
Angel <angel.ho@ryerson.ca>
2020-07-21 22:58:25 +0000

    added eqArrays in function aserArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function (arr1, arr2) {
+  let equal = true
+  if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
+    equal = false
+    return equal;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        equal = true;
+      } else {
+        equal = false
+      }
+    }
+  }
+  return equal;
+};
+
+function assertArraysEqual(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 4]))

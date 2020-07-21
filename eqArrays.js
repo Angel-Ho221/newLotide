a6b2cf81abde430176e776aa6795fb01f4943734
Angel <angel.ho@ryerson.ca>
2020-07-21 21:30:05 +0000

    function

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  let equal = true
+  if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
+    equal = false
+    return equal;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        equal = true;
+      } else {
+        equal = false
+      }
+    }
+  }
+  return equal;
+}
+
+assertEqual(eqArrays([1], [2, 3]), false);

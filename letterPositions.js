01ba6468ec539f37c4080f74a528a7892ced6423
Angel <angel.ho@ryerson.ca>
2020-07-22 20:16:42 +0000

    function for letter position

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const eqArrays = function (arr1, arr2) {
+  let equal = true
+  if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
+    equal = false
+    return equal;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        equal = true;
+      } else {
+        equal = false
+      }
+    }
+  }
+  return equal;
+};
+
+function assertArraysEqual(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (sentence[i] in results) {
+        results[sentence[i]].push(i);
+      } else if (!(sentence[i] in results)) {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+}

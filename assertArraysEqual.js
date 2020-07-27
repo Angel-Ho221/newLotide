20382c25e5394b656335aebb09b7d1e697307dcb
Angel <angel.ho@ryerson.ca>
2020-07-27 18:31:54 +0000

    refracted and separated codes and input export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  let equal = true
-  if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
-    equal = false
-    return equal;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        equal = true;
-      } else {
-        equal = false
-      }
-    }
-  }
-  return equal;
-};
+const eqArrays = require('./eqArrays');
 
 function assertArraysEqual(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -23,4 +8,4 @@ function assertArraysEqual(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3])
+module.exports = assertArraysEqual;
